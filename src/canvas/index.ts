117225929a2628d39ea55feaf11be3b2b956863f
Ben Keys <bkeys818@gmail.com>
2022-02-20 14:32:54 -0500

    🏷️ Clean up Course and Assignment types

--- src/canvas/index.ts
+++ src/canvas/index.ts
@@ -61,36 +61,14 @@ export interface Course {
 
 export interface Assignment {
     id: number
+    name: string
     description: string
     due_at: string | null
-    // unlock_at: null,
-    // lock_at: null,
-    // points_possible: 25,
-    // grading_type: 'points',
-    // assignment_group_id: 193963,
-    // grading_standard_id: null,
-    // peer_reviews: false,
-    // automatic_peer_reviews: false,
-    // position: 1,
-    // grade_group_students_individually: false,
-    // anonymous_peer_reviews: false,
-    // group_category_id: null,
-    // post_to_sis: false,
-    // moderated_grading: false,
-    // omit_from_final_grade: false,
-    // intra_group_peer_reviews: false,
-    // anonymous_instructor_annotations: false,
-    // anonymous_grading: false,
-    // graders_anonymous_to_graders: false,
-    // grader_count: 0,
-    // grader_comments_visible_to_graders: true,
-    // final_grader_id: null,
-    // grader_names_visible_to_final_grader: true,
-    // allowed_attempts: -1,
-    // annotatable_attachment_id: null,
-    // secure_params: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsdGlfYXNzaWdubWVudF9pZCI6IjBkMDFhZjhlLWYwYmUtNGFhOC05YWExLWE3M2VjYTA1M2ZlMCIsImx0aV9hc3NpZ25tZW50X2Rlc2NyaXB0aW9uIjoiXHUwMDNjcFx1MDAzZVdobyBhcmUgeW91PyBPbiBDYW52YXMsIGluIDUwMC04MDAgd29yZHMsIGludHJvZHVjZSB5b3Vyc2VsZiB0byB0aGUgY2xhc3MuIFlvdSBkb27igJl0IGhhdmUgdG8gdGVsbCB5b3VyIGxpZmUgc3RvcnkgdG8gdGhlc2UgcGVvcGxlLCBidXQgc2hhcmUgc29tZSB0aGluZ3MgYWJvdXQgeW91cnNlbGYgdGhhdCBjb3VsZCBiZSBiZW5lZmljaWFsIGZvciB1cyB0byBrbm93LiBEbyB5b3UgaGF2ZSBhbnkgcGV0cyB0aGF0IHlvdSBhYnNvbHV0ZWx5IGFkb3JlPyBXaGF0IGlzIHlvdXIgZmF2b3JpdGUgdGhpbmcgdG8gZWF0LCB0aGVuIHRlbGwgdXMgd2h5IHlvdSBsb3ZlIHRoYXQgZm9vZC4gV2hhdCBpcyBzb21ldGhpbmcgeW91IGxvdmUgdG8gZG8gYXQgeW91ciBob21ldG93biBvciBoZXJlIGluIE11bmNpZT8gSWYgeW91IHJ1biBvdXQgb2YgdGhpbmdzIHRvIHNheSBhYm91dCB5b3Vyc2VsZiwgdGhpbmsgb2YgYSBzdG9yeSB5b3UgbG92ZSB0byBzaGFyZSBhYm91dCB5b3Vyc2VsZiB0byBmaWxsIHVwIHlvdXIgc3BhY2UuXHUwMDNjL3BcdTAwM2UifQ.sOvCyfPLRRVZdKJptOxJRpmUXcZp5cLHtlLAvpF9D60',
+    lock_at: string | null
+    unlock_at: string | null
     course_id: number
-    name: string
+    html_url: string
+    points_possible: number
     submission_types: (
         | 'discussion_topic'
         | 'online_quiz'
@@ -102,72 +80,26 @@ export interface Assignment {
         | 'online_upload'
     )[]
     has_submitted_submissions: boolean
-    due_date_required: boolean
-    // max_name_length: 255,
-    // in_closed_grading_period: false,
-    // is_quiz_assignment: false,
-    // can_duplicate: true,
-    // original_course_id: null,
-    // original_assignment_id: null,
-    // original_assignment_name: null,
-    // original_quiz_id: null,
-    // workflow_state: 'published',
-    // important_dates: false,
-    // muted: true,
-    html_url: string
-    // published: true,
-    // only_visible_to_overrides: false,
-    locked_for_user: boolean
-    // submissions_download_url: 'https://bsu.instructure.com/courses/115689/assignments/1001340/submissions?zip=1',
-    // post_manually: false,
-    // anonymize_students: false,
-    require_lockdown_browser: boolean
+    grading_type:
+        | 'pass_fail'
+        | 'percent'
+        | 'letter_grade'
+        | 'gpa_scale'
+        | 'points'
+    quiz_id?: number
+    /**
+     * The number of submission attempts a student can make for this assignment.
+     * -1 is considered unlimited.
+     */
+    allowed_attempts: number
+    in_closed_grading_period?: boolean
+    is_quiz_assignment?: boolean
+    important_dates?: boolean
+    muted?: boolean
+    locked_for_user?: boolean
+    submissions_download_url?: string
+    require_lockdown_browser?: boolean
 }
-
-// interface discussionTopic {
-//       assignment_id: 1001340,
-//       id: 678259,
-//       root_topic_id: null,
-//       delayed_post_at: null,
-//       lock_at: null,
-//       title: 'Writing: Introductions',
-//       last_reply_at: '2021-08-28T02:34:28Z',
-//       created_at: '2021-08-20T19:18:52Z',
-//       posted_at: '2021-08-20T20:59:14Z',
-//       position: null,
-//       podcast_has_student_posts: false,
-//       discussion_type: 'side_comment',
-//       allow_rating: false,
-//       only_graders_can_rate: false,
-//       sort_by_rating: false,
-//       is_section_specific: false,
-//       user_name: 'Cassidy Forbing',
-//       discussion_subentry_count: 27,
-//       permissions: [Object],
-//       require_initial_post: true,
-//       user_can_see_posts: true,
-//       podcast_url: null,
-//       read_state: 'read',
-//       unread_count: 20,
-//       subscribed: true,
-//       attachments: [],
-//       published: true,
-//       can_unpublish: false,
-//       locked: false,
-//       can_lock: true,
-//       comments_disabled: false,
-//       author: [Object],
-//       html_url: 'https://bsu.instructure.com/courses/115689/discussion_topics/678259',
-//       url: 'https://bsu.instructure.com/courses/115689/discussion_topics/678259',
-//       pinned: false,
-//       group_category_id: null,
-//       can_group: false,
-//       topic_children: [],
-//       group_topic_children: [],
-//       locked_for_user: false,
-//       message: '<p>Who are you? On Canvas, in 500-800 words, introduce yourself to the class. You don’t have to tell your life story to these people, but share some things about yourself that could be beneficial for us to know. Do you have any pets that you absolutely adore? What is your favorite thing to eat, then tell us why you love that food. What is something you love to do at your hometown or here in Muncie? If you run out of things to say about yourself, think of a story you love to share about yourself to fill up your space.</p>',
-//       todo_date: null
-// }
 
 export function extractId(urlStr: string): ItemId {
     const url = new URL(urlStr)
