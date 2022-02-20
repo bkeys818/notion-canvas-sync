f12bd5fa8385b306b81d75b9c5e6990bf1042179
Ben Keys <bkeys818@gmail.com>
2022-02-20 12:02:36 -0500

    🔥 Remove 'Custom Props' property

--- src/notion/items/course.ts
+++ src/notion/items/course.ts
@@ -1,5 +1,4 @@
 import { Item } from 'notion-databases'
-import { datesAreEqual } from '../date'
 import { getRichText } from '../utils'
 import { extractId } from '../../canvas'
 import type { Course as CanvasCourse } from '../../canvas'
@@ -8,30 +7,14 @@ export default class Course extends Item<CourseProps> {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     readonly canvasUrl = this.properties['Canvas Url'].url!
     readonly canvasId = extractId(this.canvasUrl)
-    readonly customProps = (() => {
-        const keys = Object.keys(this.properties)
-        return this.properties['Custom Props'].multi_select
-            .map(obj => obj.name)
-            .filter((key): key is keyof Course['properties'] =>
-                keys.includes(key)
-            )
-    })()
     private readonly name = getRichText(this.properties.Name.title)
     private readonly duration = this.properties.Duration.date
 
     updateWith(course: CanvasCourse) {
         const props: Parameters<typeof this.update>[0] = {}
-
-        if (!this.name || !this.customProps.includes('Name'))
+        if (!this.name)
             props.Name = { title: [{ text: { content: course.name } }] }
-        if (
-            !this.customProps.includes('Duration') &&
-            course.start_at != null &&
-            datesAreEqual(this.duration, {
-                start: course.start_at,
-                end: course.end_at ?? undefined,
-            })
-        )
+        if (!this.duration && course.start_at)
             props.Duration = {
                 date: {
                     start: course.start_at,
@@ -52,6 +35,5 @@ type CourseProps = {
     Sync: 'checkbox'
     'Canvas Url': 'url'
     'Last Synced': 'date'
-    'Custom Props': 'multi_select'
     Error: 'rich_text'
 }
